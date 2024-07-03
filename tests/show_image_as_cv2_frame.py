89fcec71e153771f633a968c4775c3191cdf0025
Erdem <erdemcanaz@gmail.com>
2024-07-03 11:17:09 +0300

    Update show_image_as_cv2_frame.py

--- tests/show_image_as_cv2_frame.py
+++ tests/show_image_as_cv2_frame.py
@@ -1,12 +1,14 @@
-import cv2, datetime
+import cv2
 from pathlib import Path
 
-script_path = Path(__file__).absolute()
-script_folder = script_path.parent.absolute()
 
-media_path = script_folder / "test_image.png"
-image = cv2.imread(str(media_path))
+if __name__ == "__main__":
+    script_path = Path(__file__).absolute()
+    script_folder = script_path.parent.absolute()
 
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    media_path = script_folder / "test_image.png"
+    image = cv2.imread(str(media_path))
+
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
