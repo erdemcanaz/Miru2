7535ea692b52e28ca477ac751f005b414fdcd2b2
Erdem <erdemcanaz@gmail.com>
2024-07-03 11:08:58 +0300

    Update show_image_as_cv2_frame.py

--- tests/show_image_as_cv2_frame.py
+++ tests/show_image_as_cv2_frame.py
@@ -6,4 +6,7 @@ script_folder = script_path.parent.absolute()
 
 media_path = script_folder / "test_image.png"
 image = cv2.imread(str(media_path))
+
 cv2.imshow("Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
