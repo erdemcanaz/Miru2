8a1f40d0e70f9c968e5802eae01b71a3fe319248
Erdem <erdemcanaz@gmail.com>
2024-07-03 09:50:12 +0300

    testing scripts for docker container

---
+++ tests/test_equipment_detection_model.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+import cv2, datetime
+from pathlib import Path
+
+script_path = Path(__file__).absolute()
+script_folder = script_path.parent.absolute()
+miru2_folder = script_folder.parent.absolute()
+
+
+model_path = miru2_folder / "trained_yolo_models" / "net_google_mask_28_06_2024.pt"
+media_path = script_folder / "test_image.png"
+model = YOLO(model_path)  # pretrained YOLOv8n model
+results = model(source=media_path, vid_stride=1, show=False, save=False, project=None, conf=0.5)  # return a list of Results objects
